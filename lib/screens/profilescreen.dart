489470b3989e5ab1f639f11cddad2d7b951036c4
vishalsojitra <vishal.sojitra@drcsystems.com>
2021-01-20 10:37:26 +0530

    commit

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:flutter_gradient_colors/flutter_gradient_colors.dart';
+import 'package:myvideo/variable.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -8,11 +11,30 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  String username = '';
+  bool dataisthere = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  getUserData() async {
+    DocumentSnapshot userdoc = await usercollection.doc(FirebaseAuth.instance.currentUser.uid).get();
+    setState(() {
+      username = userdoc.data()['username'];
+      dataisthere = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[250],
-      body: Stack(
+      body: dataisthere == false ? Center(
+        child: CircularProgressIndicator(),
+      ) : Stack(
         children: [
           ClipPath(
             clipper: OvalBottomBorderClipper(),
@@ -39,6 +61,58 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     colorFilter: ColorFilter.mode(Colors.yellow, BlendMode.color),
                 ),
               ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 300,
+                ),
+                Text(
+                  username,
+                  style: mystyle(40, Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width/2,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: GradientColors.cherry)
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Edit Profile",
+                      style: mystyle(17, Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/2,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: GradientColors.darkOcean)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "SignOut",
+                        style: mystyle(17, Colors.white),
+                      ),
+                    ),
+                  ),
+                  onTap: (){
+                    FirebaseAuth.instance.signOut();
+                  },
+                ),
+              ],
             ),
           )
         ],
